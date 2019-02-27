32e71b39bcf04d67a8e323241491e2a8751c1caf
moreal <dev.moreal@gmail.com>
2019-02-27 13:34:57 +0900

    Disassemble method

---
+++ src/016. disassemble_method/main.rb
@@ -0,0 +1,36 @@
+class Animal
+    @content = "some"
+
+    def initialize
+        @content = "some"
+    end
+    
+    def say
+        puts @content
+    end
+end
+
+class Cat < Animal
+    def initialize
+        @content = "meow"
+    end
+end
+
+class Dog < Animal
+    def initialize
+        @content = "mungmung"
+    end
+end
+
+animal = Animal.new
+cat = Cat.new
+dog = Dog.new
+
+say = animal.method(:say)
+puts say  # STDIN: #<UnboundMethod: Cat#say>
+puts "#{say}'s owner: #{say.owner}"  # the original
+puts "#{say}'s parameters: #{say.parameters}"
+puts "parameters.length: #{say.arity}"  # parameters.length
+
+say.unbind.bind(dog).()  # STDIN: mungmung
+say.unbind.bind(cat).()  # STDIN: meow
